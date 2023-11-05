5c66eae6c38d1c8162ab4937aebe8e084c702c6c
Нагаев Алексей <nag495@yandex.ru>
2023-11-05 21:24:31 +0300

    3-5 создали файлы для секций подключили шаблоны и настроили роутинг

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "MainLayout"
+}
+</script>
+
+<template>
+    <div>
+        <div>
+
+        </div>
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Section/Index.vue
@@ -0,0 +1,16 @@
+<script>
+import MainLayout from "@/Layouts/MainLayout.vue";
+
+export default {
+    name: "Index",
+    layout: MainLayout
+}
+</script>
+
+<template>
+    <div>Index</div>
+</template>
+
+<style scoped>
+
+</style>
